74ef124cf4b3ddbb2793f45585c8b2619c366978
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-08-13 10:38:28 +0200

    one char input matching two cities - now generalized

--- src/main/java/CitySearch.java
+++ src/main/java/CitySearch.java
@@ -2,18 +2,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 public class CitySearch {
     public List<String> search(String input, List<String> cities) {
         if (cities.stream().allMatch(city -> city.contains(input)))
             return new ArrayList<>(cities);
-
-        if (cities.size() == 2 && cities.get(0).contains(input) && cities.get(1).contains(input))
-            return asList(cities.get(0), cities.get(1));
-
-        if (cities.size() == 1 && cities.get(0).contains(input))
-            return asList(cities.get(0));
 
         return Collections.singletonList("0");
     }
