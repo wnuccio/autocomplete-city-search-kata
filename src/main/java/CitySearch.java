3e9baebd32d58e9b3651d9c6af441bef029e9d04
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-08-13 09:41:01 +0200

    one character input with one character city

---
+++ src/main/java/CitySearch.java
@@ -0,0 +1,8 @@
+import java.util.Collections;
+import java.util.List;
+
+public class CitySearch {
+    public List<String> search(String input, List<String> cities) {
+        return Collections.singletonList("a");
+    }
+}
---
+++ src/test/java/CitySearchTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CitySearchTest {
+
+
+    @Test
+    void return_one_char_city_matching_one_input_char() {
+        CitySearch citySearch = new CitySearch();
+
+        List<String> output = citySearch.search("a", asList("a"));
+
+        assertEquals(asList("a"), output);
+    }
+}
