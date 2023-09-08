964d8be9d5ea3dadf75367814459ffcd7922cbeb
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-09-08 14:07:35 +0200

    move kata to a specific package

--- src/main/java/autocompletecitysearch/CitySearch.java
+++ src/main/java/autocompletecitysearch/CitySearch.java
@@ -1,3 +1,5 @@
+package autocompletecitysearch;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/test/java/autocompletecitysearch/CitySearchTest.java
+++ src/test/java/autocompletecitysearch/CitySearchTest.java
@@ -1,3 +1,5 @@
+package autocompletecitysearch;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
